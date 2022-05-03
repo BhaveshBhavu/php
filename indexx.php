c65106b9321c04892844c3303c5654aeb4aede27
Bhavesh <97734936+BhaveshBhavu@users.noreply.github.com>
2022-05-03 19:44:59 +0530

    Update indexx.php

--- indexx.php
+++ indexx.php
@@ -1,4 +1,42 @@
-<h1>bhavesh</h1>
+<!DOCTYPE html>
+<html>
+<head>
+
+  <title>HTML</title>
+  
+</head>
+
+<body>
+  <table border="1" >
+    <tr>
+      <th>name</th>
+      <th>gendar</th>
+    </tr>
+    <a href=""></a>
+    
+    
+  </table>
+  <form action="#" method="post">
+    
+    <p>farst nem</p>
+    <input type="text" name="fname">
+    
+    <br><br>
+    
+     <p>farst nem</p>
+     <input type="text" name="lname">
+    
+    <br><br>
+    
+    <input type="submit">
+  </form>
+  
+  
+  
+  
+</body>  
+</html>
+
 
 
 
